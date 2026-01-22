f60c6573221f15898d2a3a91784a915ca9bb4d0d
Illia Safonov <safonov.illia@gmail.com>
2026-01-22 20:23:06 +0100

    solution notification_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,6 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   const notification = document.createElement('div');
 
   notification.classList.add('notification', type);
-  notification.style.position = 'absolute';
   notification.style.top = `${posTop}px`;
   notification.style.right = `${posRight}px`;
 
