1bc433e17e8f131331242b76d0379bbcd1d28ea9
Illia Safonov <safonov.illia@gmail.com>
2026-01-22 20:23:30 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    notification.classList.add("notification", type);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    const header = document.createElement("h2");
+    header.classList.add("title");
+    header.textContent = title;
+    const desc = document.createElement("p");
+    desc.textContent = description;
+    notification.append(header, desc);
+    document.body.append(notification);
+    setTimeout(()=>{
+        notification.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
